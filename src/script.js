3e13a24cb9a40fec0d4351ed8b218825fa5b5b10
TunmiDev <adewoleoluwatunmise@gmail.com>
2024-08-25 07:16:48 +0100

    injected JS

--- src/script.js
+++ src/script.js
@@ -51,8 +51,30 @@ function handleSearchSubmit(event) {
     let searchInput = document.querySelector("#search-form-input");
     searchCity(searchInput.value);
   }
+
+function displayForecast(){
+   
+  let forecastElement = document.querySelector("#forecast");
+
+  let days =['Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+  let forecastHtml = "";
+  days.forEach(function(day){
+   forecastHtml= 
+   forecastHtml +
+   `   
+   <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature"><strong>15℃</strong></div> 
+    <div class="weather-forecast-temperature">9℃</div>
+    </div>`;
+  });
+ forecastElement.innerHTML = forecastHtml;
+}
   
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   searchCity("Lagos");
+  displayForecast();
 
